6424ffe86e1753c95b3972843654880e449b1696
hnazmul-iv <app@hnazmul.me>
2023-09-02 04:24:43 +0600

    added vscode theme and installed some default plugin

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,13 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.media.vim-wakatime" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.colorscheme.vscode-nvim", enabled = true },
+  -- {import = "astrocommunity.pack."}
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
