1824077362239c96cb8f3da10b680d93c2d77a63
hnazmul-iv <app@hnazmul.me>
2023-09-11 17:48:46 +0600

    config added for golang

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,6 +5,7 @@ return {
   { import = "astrocommunity.media.vim-wakatime" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.colorscheme.vscode-nvim", enabled = true },
+  { import = "astrocommunity.pack.go" },
   -- {import = "astrocommunity.pack."}
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
